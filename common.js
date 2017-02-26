ea360b0e4e72098e89305587155b4604e2d163a0
endlessdev <ysw0094@gmail.com>
2017-02-27 00:50:38 +0900

    Set specific url filter

--- common.js
+++ common.js
@@ -1,23 +1,15 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-    //console.log(JSON.stringify(details));
+
     var headers = details.requestHeaders,
         blockingResponse = {};
 
-    // Each header parameter is stored in an array. Since Chrome
-    // makes no guarantee about the contents/order of this array,
-    // you'll have to iterate through it to find for the
-    // 'User-Agent' element
     for (var i = 0, l = headers.length; i < l; ++i) {
         if (headers[i].name == 'Referer') {
             headers[i].value = 'http://viid.me/qcOmMI?utm_source=anigod.gryfindor.com&utm_medium=QL&utm_name=1';
-            console.log(location.href, headers[i].value);
             break;
         }
-        // If you want to modify other headers, this is the place to
-        // do it. Either remove the 'break;' statement and add in more
-        // conditionals or use a 'switch' statement on 'headers[i].name'
     }
 
     blockingResponse.requestHeaders = headers;
     return blockingResponse;
-}, { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking']);
+}, { urls: ["https://anigod.com/*"] }, ['requestHeaders']);
