b5dfcc62bebd8a8f5f4f5593501af8424665d946
endlessdev <ysw0094@gmail.com>
2017-02-27 00:42:24 +0900

    Add super referer to request headers when before send headers

--- common.js
+++ common.js
@@ -1,14 +1,23 @@
-var Request = new XMLHttpRequest();
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    //console.log(JSON.stringify(details));
+    var headers = details.requestHeaders,
+        blockingResponse = {};
 
-Request.open("GET", location.href);
-Request.setRequestHeader("Referrer", "http://viid.me/qcOmMI?utm_source=anigod.gryfindor.com&utm_medium=QL&utm_name=1");
-Request.addEventListener("load", onSuccess);
+    // Each header parameter is stored in an array. Since Chrome
+    // makes no guarantee about the contents/order of this array,
+    // you'll have to iterate through it to find for the
+    // 'User-Agent' element
+    for (var i = 0, l = headers.length; i < l; ++i) {
+        if (headers[i].name == 'Referer') {
+            headers[i].value = 'http://viid.me/qcOmMI?utm_source=anigod.gryfindor.com&utm_medium=QL&utm_name=1';
+            console.log(location.href, headers[i].value);
+            break;
+        }
+        // If you want to modify other headers, this is the place to
+        // do it. Either remove the 'break;' statement and add in more
+        // conditionals or use a 'switch' statement on 'headers[i].name'
+    }
 
-function onSuccess() {
-    document.execCommand('Stop');
-    console.log(this.response);
-}
-
-window.onbeforeunload = function() {
-    return alert("FUCK")
-};
+    blockingResponse.requestHeaders = headers;
+    return blockingResponse;
+}, { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking']);
